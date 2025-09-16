326a2b9a27f140ccc3d7dea0133ba7d33754bff7
Gagan S R <srgagan99@gmail.com>
2025-09-16 11:08:56 +0530

    Create loop1.py

---
+++ loop1.py
@@ -0,0 +1,11 @@
+# Example of for loop
+print("For Loop Example:")
+for i in range(1, 6):   # Runs from 1 to 5
+    print("Number:", i)
+
+# Example of while loop
+print("\nWhile Loop Example:")
+count = 1
+while count <= 5:
+    print("Count:", count)
+    count += 1
